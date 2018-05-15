e9f7cacf4827c19ed3861bcb15dd28845286b23c
shina-m <shina.ct@gmail.com>
2018-05-15 10:01:23 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,1 +1,24 @@
+import geni.portal as portal
+import geni.rspec.pg as rspec
+import geni.rspec.igext as IG
 
+pc = portal.Context()
+request = rspec.Request()
+
+tourDescription = "This profile provides a baseline Ubuntu 14.4 image"
+
+#
+# Setup the Tour info with the above description and instructions.
+#  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+
+node = request.RawPC("basenode")
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU14-64-STD"
+
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command="touch test.txt"))   
+
+# Print the RSpec to the enclosing page.
+portal.context.printRequestRSpec(request)
